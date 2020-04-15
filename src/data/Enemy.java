9e1e5579bd54c134d53b16c09ba3283be91d21b7
Samuel <slownie16@gmail.com>
2020-04-14 22:12:54 -0400

    basis engine in place

---
+++ src/data/Enemy.java
@@ -0,0 +1,13 @@
+package data;
+
+public class Enemy
+{
+    String name;
+    int stats []; // Stats in EngineNotes.txt
+
+    Enemy(String name, int stats[])
+    {
+        this.name = name;
+        this.stats = stats;
+    }
+}
---
+++ src/engine/Main.java
@@ -0,0 +1,29 @@
+package engine;
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+import data.PartyMember;
+import data.Enemy;
+
+public class Main
+{
+    public static void main (String [] args)
+    {
+        ArrayList<PartyMember> party = new ArrayList<>();
+        ArrayList<Enemy> enemies = new ArrayList<>();
+
+        // Create your Party Members and Enemies here!!
+        String p1Name = "Warrior";
+        int p1Stats [] = {};
+
+        String e1Name = "Slime";
+        int e1Stats [] = {};
+
+        // Battle Loop
+        while (true)
+        {
+
+        }
+    }
+}
